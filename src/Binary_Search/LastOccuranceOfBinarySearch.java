abcf710f8d90bf77c97828a3cdf0050e520e32d3
maurya-78 <jimaurya6386@gmail.com>
2026-04-04 13:00:47 +0530

    cds

--- src/Binary_Search/LastOccuranceOfBinarySearch.java
+++ src/Binary_Search/LastOccuranceOfBinarySearch.java
@@ -2,7 +2,7 @@ package Binary_Search;
 
 public class LastOccuranceOfBinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,3,3,4,5,6,7,8,9};
+        int[] arr = {1,2,3,3,3,4,5,6,7,8,9,10};
         int target = 3;
         int n= arr.length;
         int low=0;
---
+++ src/Strings/StringBasics.java
@@ -0,0 +1,14 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        //char[] arr={'R','A','H','U','L'};
+        Scanner sc = new Scanner(System.in);
+        //String x= "Aman is good boy, he is also my friend";
+        //String str = sc.next();  //next()->
+        String str = sc.nextLine();
+        System.out.print(str);
+    }
+}
