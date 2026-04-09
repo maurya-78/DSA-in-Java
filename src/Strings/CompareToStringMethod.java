5c86bfacce13ebfda56be5f9da919c8a7b7ea4a0
maurya-78 <jimaurya6386@gmail.com>
2026-04-09 22:41:13 +0530

    cds

---
+++ src/Strings/CompareToStringMethod.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class CompareToStringMethod {
+    public static void main(String[] args) {
+//        String a = "raghav";
+//        String b = "preet";
+        String a = "rahul";
+        String b = "rahul";
+        System.out.println(a.compareTo(b));
+    }
+}
