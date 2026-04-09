e5956f5abc3d66cd9425eda46d553a60b65cd594
maurya-78 <jimaurya6386@gmail.com>
2026-04-09 22:53:03 +0530

    cds

---
+++ src/Strings/Plus.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Plus {
+    public static void main(String[] args) {
+        String s = "bhakti";
+        s += " hi shakti hai";
+        s += 10;
+        System.out.println(s);
+    }
+}
