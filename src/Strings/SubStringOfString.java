238ce368bcb18cc4bd874ebdc2495a5a0e77ed77
maurya-78 <jimaurya6386@gmail.com>
2026-04-10 15:42:37 +0530

    cds

---
+++ src/Strings/SubStringOfString.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class SubStringOfString {
+    public static void main(String[] args) {
+        String s = "Rahul";
+        System.out.println(s);
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(0,4));
+    }
+}
