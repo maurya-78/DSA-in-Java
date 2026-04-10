167726a83fcb60bf97349e358a9ecef519bfb686
maurya-78 <jimaurya6386@gmail.com>
2026-04-10 12:49:16 +0530

    cds

---
+++ src/Strings/StringToCharArray.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        String s = "Rahul";
+        char[] arr = s.toCharArray();
+        for(char ch : arr){
+            System.out.println(ch);
+        }
+    }
+}
