ab866e37d505bd3a81f4ff1bd66e07d7085d6236
maurya-78 <jimaurya6386@gmail.com>
2026-04-14 16:36:56 +0530

    update

--- src/MultiDimensionalArrays/ColumnWithMaxSum.java
+++ src/MultiDimensionalArrays/ColumnWithMaxSum.java
@@ -5,11 +5,11 @@ public class ColumnWithMaxSum {
         int[][] arr = {{6,0,2,7,6},{1,3,7,2,2},{9,9,4,5,2}};
         int maxSum = Integer.MIN_VALUE;
 
-        for (int i = 0; i < arr[0].length; i++) {
+        for (int j = 0; j < arr[0].length; j++) {
             int column = 0;
             int sum =0;
             while(column<arr.length){
-                sum+=arr[column][i];
+                sum+=arr[column][j];
                 column++;
             }
 
