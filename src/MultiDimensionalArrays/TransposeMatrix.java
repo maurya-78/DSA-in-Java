a0bb0b8b30603373d0fd3094e3d86e1fefb76019
maurya-78 <jimaurya6386@gmail.com>
2026-04-15 16:48:56 +0530

    update

---
+++ src/MultiDimensionalArrays/TransposeMatrix.java
@@ -0,0 +1,15 @@
+package MultiDimensionalArrays;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{6,0,2,7,6},{1,3,7,2,2},{9,9,4,5,2}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        print(arr)
+    }
+}
