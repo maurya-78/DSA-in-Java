a86ee4f3973bf378d12e3181546bbda9d51fccd1
maurya-78 <jimaurya6386@gmail.com>
2026-04-17 12:45:23 +0530

    update

--- src/MultiDimensionalArrays/ArrayLists.java
+++ src/MultiDimensionalArrays/ArrayLists.java
@@ -11,8 +11,21 @@ public class ArrayLists {
         ArrayList<Integer> c = new ArrayList<>();
         c.add(4); c.add(14); c.add(46); c.add(142);
         ArrayList< ArrayList<Integer> > arr = new ArrayList<>();
-        arr.add(a); arr.add(a); arr.add(a);
+        arr.add(a); arr.add(b); arr.add(c);
 
-        System.out.println(arr);
+        //System.out.println(arr);
+//        for( int i=0;i<arr.size();i++){
+//            for(int j=0;j<arr.get(i).size();j++){
+//                System.out.print(arr.get(i).get(j)+" ");
+//            }
+//            System.out.println();
+//        }
+
+        for(ArrayList<Integer> List : arr){
+            for(int ele : List){
+                System.out.print(ele+" ");
+            }
+            System.out.println();
+        }
     }
 }
