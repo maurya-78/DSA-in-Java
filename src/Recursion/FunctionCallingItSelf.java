9bd8d4cb401a9c831d1d4e91155e0ee90118e59d
maurya-78 <jimaurya6386@gmail.com>
2026-04-21 12:38:30 +0530

    update

--- src/Recursion/FunctionCallingItSelf.java
+++ src/Recursion/FunctionCallingItSelf.java
@@ -1,12 +1,23 @@
 package Recursion;
 
+//public class FunctionCallingItSelf {
+//    public static void main(String[] args) {
+//         aman(1);
+//    }
+//    public static void aman(int n){
+//        if(n==10) return;
+//        System.out.println("Rahul");
+//        aman(n+1);
+//    }
+//}
+
 public class FunctionCallingItSelf {
     public static void main(String[] args) {
-         aman(1);
+        print(5);
     }
-    public static void aman(int n){
-        if(n==10) return;
-        System.out.println("Rahul");
-        aman(n+1);
+    public static void print(int n){
+        if(n==0) return;
+        System.out.println(n);
+        print(n-1);
     }
 }
