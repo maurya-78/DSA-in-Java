3e9b81839f8db782fb99ac3bfaf69eb3284343d4
maurya-78 <jimaurya6386@gmail.com>
2026-04-24 20:19:08 +0530

    update

--- src/Recursion/GCD.java
+++ src/Recursion/GCD.java
@@ -1,4 +1,6 @@
 package Recursion;
+import java.util.Scanner;
+
 
 public class GCD {
    static class Solution {
@@ -8,6 +10,16 @@ public class GCD {
         }
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("GCD is: " + Solution.gcd(a, b));
+
+        sc.close();
     }
 }
