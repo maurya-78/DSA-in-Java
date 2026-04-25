600fb6724dc68bd0032f084427084392b82310e0
maurya-78 <jimaurya6386@gmail.com>
2026-04-25 21:47:38 +0530

    update

---
+++ src/Recursion/RecursionOnArrays.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class RecursionOnArrays {
+    public static void main(String[] args) {
+        int[] arr = {5,3,7,12,76,9,34,5};
+        recPrint(arr,0);
+    }
+    public static void recPrint(int[] arr, int idx){
+        int n = arr.length;
+        if(idx == n) return;
+        System.out.println(arr[idx]);
+        recPrint(arr,idx+1);
+    }
+}
